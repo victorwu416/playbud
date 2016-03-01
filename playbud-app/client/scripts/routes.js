7e6710f1e774937db184efb05fe76fac19ccbf8b
Victor Wu <vwu@clm-vwu.enova.com>
2016-03-01 10:23:58 -0600

    Comment out auth code

--- playbud-app/client/scripts/routes.js
+++ playbud-app/client/scripts/routes.js
@@ -8,58 +8,58 @@ function config($stateProvider, $urlRouterProvider) {
       url: '/tab',
       abstract: true,
       templateUrl: 'client/templates/tabs.html',
-      resolve: {
-        user: isAuthorized,
-        chats() {
-          return Meteor.subscribe('chats');
-        }
-      }
+      // resolve: {
+      //   user: isAuthorized,
+      //   chats() {
+      //     return Meteor.subscribe('chats');
+      //   }
+      // }
     })
-    .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'client/templates/chats.html',
-          controller: 'ChatsCtrl as chats'
-        }
-      }
-    })
-    .state('tab.chat', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'client/templates/chat.html',
-          controller: 'ChatCtrl as chat'
-        }
-      }
-    })
-    .state('login', {
-      url: '/login',
-      templateUrl: 'client/templates/login.html',
-      controller: 'LoginCtrl as logger'
-    })
-    .state('confirmation', {
-      url: '/confirmation/:phone',
-      templateUrl: 'client/templates/confirmation.html',
-      controller: 'ConfirmationCtrl as confirmation'
-    })
-    .state('profile', {
-      url: '/profile',
-      templateUrl: 'client/templates/profile.html',
-      controller: 'ProfileCtrl as profile',
-      resolve: {
-        user: isAuthorized
-      }
-    })
-    .state('tab.settings', {
-      url: '/settings',
-      views: {
-        'tab-settings': {
-          templateUrl: 'client/templates/settings.html',
-          controller: 'SettingsCtrl as settings',
-        }
-      }
-    })
+    // .state('tab.chats', {
+    //   url: '/chats',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'client/templates/chats.html',
+    //       controller: 'ChatsCtrl as chats'
+    //     }
+    //   }
+    // })
+    // .state('tab.chat', {
+    //   url: '/chats/:chatId',
+    //   views: {
+    //     'tab-chats': {
+    //       templateUrl: 'client/templates/chat.html',
+    //       controller: 'ChatCtrl as chat'
+    //     }
+    //   }
+    // })
+    // .state('login', {
+    //   url: '/login',
+    //   templateUrl: 'client/templates/login.html',
+    //   controller: 'LoginCtrl as logger'
+    // })
+    // .state('confirmation', {
+    //   url: '/confirmation/:phone',
+    //   templateUrl: 'client/templates/confirmation.html',
+    //   controller: 'ConfirmationCtrl as confirmation'
+    // })
+    // .state('profile', {
+    //   url: '/profile',
+    //   templateUrl: 'client/templates/profile.html',
+    //   controller: 'ProfileCtrl as profile',
+    //   resolve: {
+    //     user: isAuthorized
+    //   }
+    // })
+    // .state('tab.settings', {
+    //   url: '/settings',
+    //   views: {
+    //     'tab-settings': {
+    //       templateUrl: 'client/templates/settings.html',
+    //       controller: 'SettingsCtrl as settings',
+    //     }
+    //   }
+    // })
     .state('tab.progress', {
       url: '/progress',
       views: {
@@ -100,15 +100,15 @@ function config($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('tab/progress');
 
   ////////////
-
-  function isAuthorized($q) {
-    let deferred = $q.defer();
-
-    if (_.isEmpty(Meteor.user()))
-      deferred.reject('AUTH_REQUIRED');
-    else
-      deferred.resolve();
-
-    return deferred.promise;
-  }
+  // 
+  // function isAuthorized($q) {
+  //   let deferred = $q.defer();
+  //
+  //   if (_.isEmpty(Meteor.user()))
+  //     deferred.reject('AUTH_REQUIRED');
+  //   else
+  //     deferred.resolve();
+  //
+  //   return deferred.promise;
+  // }
 }
