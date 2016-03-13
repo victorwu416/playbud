c5b8be980716d9828278bb2f3999764e01090604
Victor Wu <vwu@CLM-vwu.local>
2016-03-13 18:33:41 -0500

    use _instance, code refactoring using helpers

--- playbud-app/client/scripts/controllers/evaluate.controller.js
+++ playbud-app/client/scripts/controllers/evaluate.controller.js
@@ -2,95 +2,93 @@ angular
   .module('Playbud')
   .controller('EvaluateCtrl', EvaluateCtrl);
 
-function EvaluateCtrl ($scope, $reactive) {
+function EvaluateCtrl($scope, $reactive) {
   $reactive(this).attach($scope);
 
-  this.helpers({
-    showStart() { return this.showStart ? this.showStart: false },
-    showQuestion() { return this.showQuestion ? this.showQuestion : false; },
-    showResults() { return this.showResults ? this.showResults : false; },
-    nextButtonDisabled() { return this.nextButtonDisabled ? this.nextButtonDisabled : true; },
-    evaluationSkills() { return this.evaluationSkills ? this.evaluationSkills : []; },
-    currentQuestionIndex() { return this.currentQuestionIndex ? this.currentQuestionIndex : -1; },
-    selectedAnswerOptionValue() { return this.selectedAnswerOptionValue ? this.selectedAnswerOptionValue : ''; },
-    skillsWithAnswers() { return this.skillsWithAnswers ? this.skillsWithAnswers : []; },
+  var _instance = this;
+
+  _instance.helpers({
+    currentQuestion() {
+      return _instance.currentQuestion;
+    },
+    selectedAnswerOptionValue() {
+      return _instance.selectedAnswerOptionValue;
+    },
+    skillsWithAnswers() {
+      return _instance.skillsWithAnswers;
+    }
   });
 
-  this.toggleSection = toggleSection;
-  this.start = start;
-  this.submitAnswer = submitAnswer;
-  this.done = done;
+  _instance.start = start;
+  _instance.submitAnswer = submitAnswer;
+  _instance.done = done;
 
-  this.answerOptions = [
-    {value: 'easily', text: 'Easily'},
-    {value: 'with-difficulty', text: 'With difficulty'},
-    {value: 'unable', text: 'Unable'},
-    {value: 'did-not-try', text: 'Did not try'}
-  ];
+  _instance.evaluationSkills = [];
+  _instance.evaluationSkillsCopy = [];
+  _instance.currentQuestion = null;
+  _instance.section = 'start';
 
-  this.toggleSection('start');
+  _instance.answerOptions = [{
+    value: 'easily',
+    text: 'Easily'
+  }, {
+    value: 'with-difficulty',
+    text: 'With difficulty'
+  }, {
+    value: 'unable',
+    text: 'Unable'
+  }, {
+    value: 'did-not-try',
+    text: 'Did not try'
+  }];
 
   // Controller functions
-  function toggleSection (section) {
-    this.showStart = this.showQuestion = this.showResults = false;
-    switch (section) {
-      case 'start': this.showStart = true; break;
-      case 'question': this.showQuestion = true; break;
-      case 'results': this.showResults = true; break;
-      default: break;
-    }
-  }
-
-  function start () {
-    this.subscribe('nextSkills', function () {
-      angular.copy(Skills.find({}).fetch(), this.evaluationSkills);
-      this.currentQuestionIndex = -1;
-      nextQuestion(this);
+  function start() {
+    _instance.subscribe('nextSkills', function() {
+      angular.copy(Skills.find({}).fetch(), _instance.evaluationSkills);
+      angular.copy(_instance.evaluationSkills, _instance.evaluationSkillsCopy);
+      nextQuestion();
     });
   }
 
-  function submitAnswer () {
-    (function (evaluateCtrl) {
-      Meteor.call(
-        'submitAnswer',
-        evaluateCtrl.evaluationSkills[evaluateCtrl.currentQuestionIndex],
-        evaluateCtrl.selectedAnswerOptionValue,
-        function (error, result) {
-          if (error) {
-            throw new Meteor.Error('method-call-submitAnswer', 'Error submitting answer');
-          } else {
-            nextQuestion(evaluateCtrl);
-          }
-        });
-    })(this);
+  function submitAnswer() {
+    Meteor.call(
+      'submitAnswer',
+      _instance.currentQuestion,
+      _instance.selectedAnswerOptionValue,
+      function(error, result) {
+        if (error) {
+          throw new Meteor.Error('method-call-submitAnswer', 'Error submitting answer');
+        } else {
+          nextQuestion();
+        }
+      });
   }
 
-  function done () {
-    this.toggleSection('start');
+  function done() {
+    _instance.section = 'start';
   }
 
   // Private functions
-  function nextQuestion (evaluateCtrl) {
-    evaluateCtrl.nextButtonDisabled = true;
-    evaluateCtrl.currentQuestionIndex++;
-    if (evaluateCtrl.currentQuestionIndex >= evaluateCtrl.evaluationSkills.length) {
-      results(evaluateCtrl);
+  function nextQuestion() {
+    _instance.nextButtonDisabled = true;
+    _instance.currentQuestion = _instance.evaluationSkills.shift();
+    if (_instance.currentQuestion) {
+      _instance.selectedAnswerOptionValue = '';
+      _instance.section = 'question';
     } else {
-      evaluateCtrl.selectedAnswerOptionValue = '';
-      evaluateCtrl.toggleSection('question');
+      results();
     }
   }
 
-  function results (evaluateCtrl) {
-    (function (evaluateCtrl) {
-      Meteor.call('skillsWithAnswers', evaluateCtrl.evaluationSkills, function (error, skillsWithAnswers) {
-        if (error) {
-          throw new Meteor.Error('method-call-skillsWithAnswers', 'Error getting skills with answers');
-        } else {
-          evaluateCtrl.skillsWithAnswers = skillsWithAnswers;
-          evaluateCtrl.toggleSection('results');
-        }
-      });
-    })(evaluateCtrl);
+  function results() {
+    Meteor.call('skillsWithAnswers', _instance.evaluationSkillsCopy, function(error, skillsWithAnswers) {
+      if (error) {
+        throw new Meteor.Error('method-call-skillsWithAnswers', 'Error getting skills with answers');
+      } else {
+        _instance.skillsWithAnswers = skillsWithAnswers;
+      }
+    });
+    _instance.section = 'results';
   }
 }
