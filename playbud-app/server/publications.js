92f76ac226b1a045812e45b2d7f0ba890930358f
Victor Wu <vwu@CLM-vwu.local>
2016-03-25 08:45:34 -0500

    remove debug comments

--- playbud-app/server/publications.js
+++ playbud-app/server/publications.js
@@ -33,36 +33,3 @@ Meteor.publishComposite('skills', function (bottomMonths) {
     ]
   };
 });
-//
-//
-// Meteor.publishComposite('skills', function (which, specificSkills) {
-//   if (!this.userId) {
-//     throw new Meteor.Error('not-logged-in', 'Must be logged in to publish skills');
-//   }
-//   check(which, String);
-//   check(specificSkills, Array);
-//   return {
-//     find: function() {
-//       var selector = {};
-//       if (which === 'next') {
-//         selector = {};
-//       } else if (which === 'specific') {
-//         var skillIds = _.map(specificSkills, function (skill) {
-//           return skill._id;
-//         });
-//         selector = {_id: {$in: skillIds}}; // TODO: Logic to return skills only within the age range.
-//       } else {
-//         throw new Meteor.Error('invalid-which-argument', 'Invalid which argument, where which is: ' + which);
-//       }
-//       return Skills.find(selector, {limit: 5});
-//     },
-//     children: [
-//       {
-//         collectionName: "skillAnswers",
-//         find: function (skill) {
-//           return Answers.find({skillId: skill._id.valueOf()});
-//         }
-//       }
-//     ]
-//   };
-// });
