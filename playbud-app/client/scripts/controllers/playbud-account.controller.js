b67cab69c1e6127077eea289bdd5a90148625571
Victor Wu <vwu@clm-vwu.enova.com>
2016-04-01 12:58:05 -0500

    sign up validations

--- playbud-app/client/scripts/controllers/playbud-account.controller.js
+++ playbud-app/client/scripts/controllers/playbud-account.controller.js
@@ -12,22 +12,34 @@ function PlaybudAccountCtrl($reactive, $scope, $stateParams) {
     },
     loggedOutSection() {
       return _instance.loggedOutSection;
+    },
+    showSignUpValidationMessage() {
+      return _instance.showSignUpValidationMessage;
+    },
+    signUpValidationMessage() {
+      return _instance.signUpValidationMessage;
     }
   });
 
-  _instance.loggedOutSection = $stateParams.loggedOutSection
+  _instance.loggedOutSection = $stateParams.loggedOutSection;
+  _instance.showSignUpValidationMessage = false;
 
   _instance.createPlaybudAccountAndLogIn = function () {
+    if (!clientValidateSignUp()) {
+      return;
+    }
     Meteor.call(
       'createPlaybudAccount',
       _instance.email,
       _instance.password,
       _instance.childName,
       _instance.childBirthdate,
-      function(error) {
+      function(error, result) {
         if (error) {
-          throw new Meteor.Error('method-call-createPlaybudAccount', 'Error creating Playbud account');
+          _instance.signUpValidationMessage = 'Email already exists. Cancel and log in';
+          _instance.showSignUpValidationMessage = true;
         } else {
+          _instance.showSignUpValidationMessage = false;
           _instance.logIn();
         }
       }
@@ -50,5 +62,28 @@ function PlaybudAccountCtrl($reactive, $scope, $stateParams) {
         throw new Meteor.Error('meteor-loginWithPassword', 'Error logging in with password');
       }
     });
+  }
+
+  _instance.cancel = function () {
+    _instance.showSignUpValidationMessage = true;
+    _instance.loggedOutSection='signUpLogIn';
+  }
+
+  function clientValidateSignUp () {
+    _instance.signUpValidationMessage = '';
+    if (!_instance.email) {
+      _instance.signUpValidationMessage = 'Enter valid email';
+    } else if (!_instance.password) {
+      _instance.signUpValidationMessage = 'Enter password';
+    } else if (!_instance.childName) {
+      _instance.signUpValidationMessage = 'Enter child\'s name';
+    } else if (!_instance.childBirthdate) {
+      _instance.signUpValidationMessage = 'Enter valid child\'s birthdate';
+    }
+    if (_instance.signUpValidationMessage) {
+      _instance.showSignUpValidationMessage = true;
+      return false;
+    }
+    return true;
   }
 }
