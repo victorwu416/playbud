a8e2ddf77399fe6e44ae51d1d609b9f1728b84ab
Victor Wu <vwu@clm-vwu.enova.com>
2016-04-05 13:44:30 -0500

    loading mask when creating trial account

--- playbud-app/client/scripts/controllers/first.controller.js
+++ playbud-app/client/scripts/controllers/first.controller.js
@@ -6,7 +6,16 @@ function FirstCtrl($reactive, $scope, $state) {
   var _instance = this;
   $reactive(_instance).attach($scope);
 
+  _instance.helpers({
+    creatingEphemeralAccount() {
+      return _instance.creatingEphemeralAccount;
+    },
+  });
+
+  _instance.creatingEphemeralAccount = false;
+
   _instance.createEphemeralAccount = function () {
+    _instance.creatingEphemeralAccount = true;
     Meteor.call(
       'createPlaybudAccount',
       'playbud' + Math.random() + '@playbud.me',
@@ -19,6 +28,7 @@ function FirstCtrl($reactive, $scope, $state) {
         } else {
           Session.set('ephemeralUserId', ephemeralUserId);
           $state.go('tab.play');
+          _instance.creatingEphemeralAccount = false;
         }
       }
     );
